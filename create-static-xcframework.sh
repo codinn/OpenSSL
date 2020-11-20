1aa26d2b91696142a2599740e74eb7619287e427
Yang.Y <yang@yangyubo.com>
2020-11-20 15:39:00 +0800

    Support build compact XCFramework (iOS and macOS)

--- create-static-xcframework.sh
+++ create-static-xcframework.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
+set -euo pipefail
+
+if [ $# == 0 ]; then
+    echo "Usage: `basename $0` full|compact"
+    exit 1
+fi
+
+if [ ! -d lib ]; then
+    echo "Please run build-libssl.sh first!"
+    exit 1
+fi
+
+BUILD_TYPE=$1
 FWNAME=OpenSSL
 FWROOT=frameworks
 LIBNAME=openssl
 ARGS=
 XCFRAMEWORK_DEPS=
-ALL_SYSTEMS=("iPhoneOS" "iPhoneSimulator" "AppleTVOS" "AppleTVSimulator" "MacOSX" "Catalyst" "WatchOS" "WatchSimulator")
+
+if [ $BUILD_TYPE == "full" ]; then
+    ALL_SYSTEMS=("iPhoneOS" "iPhoneSimulator" "AppleTVOS" "AppleTVSimulator" "MacOSX" "Catalyst" "WatchOS" "WatchSimulator")
+else
+    ALL_SYSTEMS=("iPhoneOS" "iPhoneSimulator" "MacOSX")
+fi
 
 if [ -d $FWROOT ]; then
     echo "Removing previous $FWNAME.xcframework and intermediate files"
-    rm -rdf $FWROOT
+    rm -rf $FWROOT
 fi
 
 for SYS in ${ALL_SYSTEMS[@]}; do
+    echo "Creating universal static library for $SYS"
     SYSDIR="$FWROOT/$SYS"
 	SYSDISTS=(bin/${SYS}*)
 	LIPO_LIBS=
@@ -28,4 +47,9 @@ for SYS in ${ALL_SYSTEMS[@]}; do
 	XCFRAMEWORK_DEPS+=" $SYSDIR/libopenssl.a"
 done
 
+echo "Creating xcframework"
 xcodebuild -create-xcframework $ARGS -output "$FWROOT/$FWNAME.xcframework"
+
+echo "Packing and computing checksum"
+zip -rq "$FWROOT/$FWNAME.xcframework.zip" "$FWROOT/$FWNAME.xcframework"
+swift package compute-checksum frameworks/OpenSSL.xcframework.zip
