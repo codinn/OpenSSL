ee89b899fdb7d5aaac6810fadbdacb008622c6f9
Yang.Y <yang@yangyubo.com>
2024-07-23 10:05:43 +0800

    Version 3.0.14-p1

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/codinn/OpenSSL/releases/download/3.0.14/openssl.xcframework.zip",
+            url: "https://github.com/codinn/OpenSSL/releases/download/3.0.14-p1/openssl.xcframework.zip",
             checksum: "fa6cdf83eb7996890bdc08f183e0ad6b24f29dcb78520cce241157b4ce1586bc"
         ),
         .target(name: "OpenSSL",
